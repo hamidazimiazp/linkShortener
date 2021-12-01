fb93ade45d0d374f01ddde2be0d924c86e290f73
Hamid Azimi <hamidazimi.azp@gmail.com>
2021-12-01 15:20:00 +0330

    create shortenet model in base app

--- linkShortener/base/models.py
+++ linkShortener/base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Shortener(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    times_follow = models.PositiveIntegerField(default=0)
+    long_url = models.URLField(max_length=200)
+    short_url = models.CharField(max_length=15, unique=True, blank=True)
+    
+    
+    def __str__(self):
+        return f"{self.short_url} | {self.long_url}"
+    
+    
+    class Meta:
+        ordering = ['-created']
